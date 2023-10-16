e2d9cf93b5595d8035f114e44588a6f40012d4f5
Michel Lesoinne <michel.lesoinne@androdevllc.com>
2023-10-15 20:21:46 -0700

    chore: Remove unused server routes.
    The app does not need the tile server anymore. The tiles come from
    a zip file served from the static folder.

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,15 +1,3 @@
 /** @type {import('./$types').PageLoad} */
-//export async function load({ fetch }) {
-// 	return {
-// 		status: 400,
-// 		databaseIsOK: false
-// 	}
-	// const response = await fetch('/tiles/okdb.json');
-	// return {
-	// 	status: response.status,
-	// 	databaseIsOK: response.ok
-	// };
-// }
-
 
 export const prerender = true;
