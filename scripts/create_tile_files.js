2cb47f9ee210c7ffc3764edab66c27c10b45cbae
Michel Lesoinne <michel.lesoinne@androdevllc.com>
2023-10-16 10:20:33 -0700

    chore: improve tile extraction script and the README.

--- scripts/create_tile_files.js
+++ scripts/create_tile_files.js
@@ -2,16 +2,22 @@ import Database from 'better-sqlite3';
 import pako from 'pako';
 import * as fs from 'fs';
 import { program } from 'commander';
-
+let filename = '';
 program
     .version('1.0.0', '-v, --version')
     .option('-f, --from <level>', 'Give the minimum level', '0')
     .option('-t, --to <level>', 'the maximum level', '6')
-    .usage('[OPTIONS]...');
+    .argument('[mbtile-filename]', 'file with mbtile data', 'tile_data/planet.mbtiles')
+    .usage('[OPTIONS]...')
+    .action((fn) => {
+        console.log('here', fn);
+        filename = fn;
+    });
 
 program.parse();
 
-console.log('options', program.opts())
+// console.log('options', program.opts(), 'arguments', program.args)
+
 const options = program.opts();
 
 const fromLevel = parseInt(options.from);
@@ -21,10 +27,13 @@ console.log(`From ${fromLevel} to ${toLevel}`)
 const dest_dir = './tile_files';
 
 let read;
+let levelRead;
 try {
-    const db = new Database('./tile_data/planet.mbtiles');
+    const db = new Database(filename);
     read = db.prepare("SELECT HEX(tile_data) as tile_data_hex FROM tiles " +
         "WHERE zoom_level = ? AND tile_column = ? AND tile_row = ? limit 1");
+    levelRead = db.prepare("SELECT tile_column, tile_row, HEX(tile_data) as tile_data_hex FROM tiles " +
+        "WHERE zoom_level = ?");
 } catch (error) {
     console.error('Failed to open database:', error);
     process.exit(1);
@@ -38,8 +47,8 @@ try {
 }
 
 for (let level = fromLevel; level <= toLevel; ++level) {
-    console.log(`All good! Extracting ${1 << (2 * level)} files`);
-
+    console.log(`Extracting files at level ${level}`);
+    let count = 0;
     function write(z, x, y, blob) {
         let dir_path = `${dest_dir}/${z}/${x}`;
         let file_path = `${dir_path}/${y}.pbf`;
@@ -54,26 +63,16 @@ for (let level = fromLevel; level <= toLevel; ++level) {
     }
 
     let z = level;
-    for (let x = 0; x < (1 << level); ++x)
-        for (let y = 0; y < (1 << level); ++y) {
-            try {
-                const result = read.get(z, x, (1 << level) - y - 1);
-
-                if (!result || !result.tile_data_hex) {
-                    console.error(`Could not read database data at ${z} ${x} ${y}`, result);
-                    process.exit(1);
-                }
-                const hexData = result.tile_data_hex;
-
-
-                let binData = new Uint8Array(hexData.match(/.{1,2}/g).map((byte) => parseInt(byte, 16)));
-                let isGzipped = binData[0] === 0x1f && binData[1] === 0x8b;
-                if (isGzipped)
-                    binData = pako.inflate(binData);
-                write(z, x, y, binData);
-            } catch (err) {
-                console.error('Failed to read database:', err);
-                process.exit(1);
-            }
-        }
+    for (const tileData of levelRead.iterate(level)) {
+        const { tile_column, tile_row, tile_data_hex } = tileData;
+        const x = parseInt(tile_column);
+        const y = (1 << level) - parseInt(tile_row) - 1;
+        let binData = new Uint8Array(tile_data_hex.match(/.{1,2}/g).map((byte) => parseInt(byte, 16)));
+        let isGzipped = binData[0] === 0x1f && binData[1] === 0x8b;
+        if (isGzipped)
+            binData = pako.inflate(binData);
+        write(z, x, y, binData);
+        ++count;
+    }
+    console.log(`Extracted ${count} tiles at level ${level}`);
 }
